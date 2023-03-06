fd57900f7a577c56295e8b909a530b8cd7a3528d
EGorio8 <90909921+EGorio8@users.noreply.github.com>
2023-03-06 21:32:01 +0300

    Update main10.java

--- first project/src/main10.java
+++ first project/src/main10.java
@@ -1,19 +1,18 @@
 import java.util.Scanner;
+
 public class main10 {
-    public static void main (String[] args) {
-        Scanner sc = new Scanner (System.in);
-        int x = sc.nextInt();
-        String output;
-        for (int i = 0; i < x+1; i++) {
-            output = main9.print_line(i);
-            System.out.println(output);
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        int n = sc.nextInt();
+        String out = "*";
+        String space = "";
+        for (var i = 1; i <= n; i++) {
+            for (var j = n; j > i; j--) {
+                space += " ";
+            }
+            System.out.println(space + out);
+            out += "**";
+            space = "";
         }
-    }
-    public static String print_line(int length) {
-        String str = "";
-        for (int i = 0; i < length; i++) {
-            str += "*";
-        }
-        return str;
     }
 }
